38d600df4dcf87c1945a620887efdd2c459f0f5a
Sydney Palmer <112440108+Sydney-Palmer@users.noreply.github.com>
2022-10-10 22:04:37 +0000

    Chapter 5 Exercise redo

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -9,16 +9,16 @@ def star_pyramid():
     print (i * "*")
     
  
-    
-    
-  
-    
-    
-   
-    
-
-
-
-
 star_pyramid()
 
+
+def rstar_pyramid():
+  print ("How many rows do you want in your star pyramid?: ")
+  value_2 = int(input(""))
+
+  for i in range (value_2, 0, -1):
+
+    print (i* "*")
+
+
+rstar_pyramid()
